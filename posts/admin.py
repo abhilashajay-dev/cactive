0769a546cd751d6b06cbb8e7b09c341ff7ee722b
abhilashajay-dev <55262545+abhilashajay-dev@users.noreply.github.com>
2020-12-08 21:37:11 +0530

    database logic

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Album, AlbumImages
 
 # Register your models here.
+
+class AlbumImagesAdmin(admin.StackedInline):
+	model =AlbumImages
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+	inline = [AlbumImagesAdmin]
+
+	class Meta:
+		model = Album	
+
+@admin.register(AlbumImages)
+class AlbumImagesAdmin(admin.ModelAdmin):
+	pass
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField(null=True, blank=True)
+	image = models.ImageField(upload_to="album/image", null=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+class AlbumImages(models.Model):
+	album = models.ForeignKey(Album, default=None, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to="album/images", null=True, blank=True)
+
+	def __str__(self):
+		return self.Album.title		
